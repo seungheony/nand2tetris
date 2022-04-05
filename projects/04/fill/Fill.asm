706f592068fb6acb4c136933d0f9e347a1401cb8
shkim <shkim@nshc.net>
2022-04-05 17:44:00 +0900

    to chapter4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,46 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@i
+M=0     // i = 0 초기화
+
+(LOOP)
+
+    @KBD
+    D=M     // 현재 키보드에 입력된 값을 D에 대입
+
+    @NOINPUT
+    D;JEQ   // D == 0 jump
+    @INPUT
+    D;JGT   // D > 0 jump
+
+(NOINPUT)
+    @16384      // 
+    D=A         //
+    @i          // 현재 스크린 위치 -> A
+    D=D+M       //
+    A=D         //
+
+    M=0         // 현재 스크린위치에 0000 0000 0000 0000 대입
+
+    @END
+    0;JMP
+(INPUT)
+    @16384      // 
+    D=A         //
+    @i          // 현재 스크린 위치 -> A
+    D=D+M       //
+    A=D         //
+
+    M=-1        // 현재 스크린 위치에 1111 1111 1111 1111 대입
+
+    @END
+    0;JMP
+(END)
+    @i
+    M=M+1       // i = i + 1
+
+    @LOOP
+    0;JMP       // LOOP로 jump
+    
