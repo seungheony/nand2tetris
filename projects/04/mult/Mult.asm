cc7adb5e4f64c0898712c210044215aefc5109ed
shkim <shkim@nshc.net>
2022-04-05 10:36:50 +0900

    to chapter4-multi

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,21 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @2
+    M=0
+(LOOP)
+    @0
+    D=M
+    @END
+    D;JEQ
+    @1
+    D=M
+    @2 
+    M=M+D
+    @0
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
